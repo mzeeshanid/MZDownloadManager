a3705c1fced847a492b0b3fa0ef968a8cfd7e0d2
Muhammad Zeeshan <mzeeshanid@Muhammads-Mac-Pro.local>
2012-11-08 10:07:07 +0500

    Initial Commit

---
+++ MyDownloadManager/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyDownloadManager
+//
+//  Created by Muhammad Zeeshan on 11/8/12.
+//  Copyright (c) 2012 FUUAST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MyDownloadManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyDownloadManager
+//
+//  Created by Muhammad Zeeshan on 11/8/12.
+//  Copyright (c) 2012 FUUAST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
