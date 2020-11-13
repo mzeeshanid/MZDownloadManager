801339376d69195b9be5028a4ff0e48bfda83114
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2020-11-13 13:04:37 +0500

    - sources path updated

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MZDownloadManager",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "MZDownloadManagerTests",
-            dependencies: ["MZDownloadManager"]),
+            dependencies: ["MZDownloadManager"],
+            path: "Tests"),
     ]
 )
